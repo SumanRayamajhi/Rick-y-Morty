dc3f32c76b3c1bcc08f3dcaaaeaf385fe507f727
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2023-11-16 23:53:36 +0100

    add necessary pakages, delete unwanted files and create components

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center title my-5">
+        {" "}
+        <span className="RM">R</span>ick Y <span className="RM">M</span>orty
+      </h1>
+    </div>
+  );
+}
+
+export default App;
