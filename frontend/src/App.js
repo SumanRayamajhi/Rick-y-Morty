f26c78478db85b331ba5a00c362cf7c68a1a4fe3
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2023-11-17 02:25:20 +0100

    add card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,30 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+import Cards from "./components/Cards/Cards";
 import "./App.css";
 
 function App() {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [fetchData, setFetchData] = useState([]);
+  const { info, results } = fetchData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    getRickYMortyApi();
+  }, [api]);
+
+  const getRickYMortyApi = async () => {
+    const response = await fetch(api);
+    const data = await response.json();
+    setFetchData(data);
+  };
   return (
     <div className="App">
-      <h1 className="text-center title my-5">
+      <h1 className="title">
         {" "}
         <span className="RM">R</span>ick Y <span className="RM">M</span>orty
       </h1>
+      <Cards results={results} />
     </div>
   );
 }
---
+++ frontend/src/components/Cards/Cards.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./cards.css";
+
+const Cards = ({ results }) => {
+  console.log(results);
+  let display;
+  if (results) {
+    display = results.map(({ name, image, species, origin }, index) => {
+      return (
+        <div className="card__container" key={index}>
+          <div className="cards__content">
+            <img src={image} alt="characters" className="characters-img" />
+            <div className="container-content">
+              <h2 className="characters-name">{name}</h2>
+              <h3 className={species === "Human" ? "human" : "non-human"}>
+                {species}
+              </h3>
+              <h3 className="origin-name">{origin.name}</h3>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  } else {
+    display = "No Characters Found";
+  }
+  return <section className="cards">{display}</section>;
+};
+
+export default Cards;
